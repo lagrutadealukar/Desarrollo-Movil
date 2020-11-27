b49c8975e65a1ec6cda3f960c8fc238f93033f02
lagrutadealukar <hollmancruz23@gmail.com>
2020-11-26 17:07:52 -0800

    Variables

---
+++ kotlin/practica1/Variables.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    var nombre = "Hollman"
+    //nombre = "Rodolfo"
+    println("mi nombre es $nombre ")
+    val apellido = "Cruz"
+    //apellido = "Centeno"
+    println("mi apellido es $apellido")
+    var edad = 23
+    println("mi edad es $edad")
+    println ("mi nombre es $nombre $apellido y tengo $edad años")
+
+    
+}
